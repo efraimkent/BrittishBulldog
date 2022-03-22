1e46c6b713bddfbd25ee1bd1213a0e19c4cadbe8
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2022-03-22 22:23:57 +0100

    Edits Game script

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
 public class Game : MonoBehaviour {
     public State currentState;
+    public GameView view;
+
+    void OnEnable() {
+        view.UpdateView(currentState);
+    }
 }
